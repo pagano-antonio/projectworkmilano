aa01f00b1ed152b7d6b5a7682f0a124f71567f11
SimonaDF <simonadf90@gmail.com>
2024-03-07 18:01:20 +0100

    WorkExperienceCtr

--- ProjectWorkMilanoBE/src/main/java/com/Ctr/WorkExperienceCtr.java
+++ ProjectWorkMilanoBE/src/main/java/com/Ctr/WorkExperienceCtr.java
@@ -1,17 +1,10 @@
 package com.Ctr;
 
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import com.dao.WorkExperienceRepository;
 
 
 @Controller
 public class WorkExperienceCtr {
-	
-	@Autowired
-	private WorkExperienceRepository WorkExperienceRepository;
-	
 
 }
