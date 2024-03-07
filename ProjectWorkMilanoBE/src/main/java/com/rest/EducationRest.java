46f1b7e444f66fb17e62f93d40aa7e6002321b65
rubinof03 <fra.rubinolato@yahoo.it>
2024-03-07 18:15:04 +0100

    mi ero scordata di cancellare una cosa, tutto apposto.

--- ProjectWorkMilanoBE/src/main/java/com/rest/EducationRest.java
+++ ProjectWorkMilanoBE/src/main/java/com/rest/EducationRest.java
@@ -1,7 +1,5 @@
 package com.rest;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dao.EducationRepository;
-import com.model.CompanyClient;
 import com.model.Education;
 
 @RestController
