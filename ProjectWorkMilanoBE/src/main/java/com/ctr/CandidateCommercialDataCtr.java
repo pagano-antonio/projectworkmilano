6e0b8c45775369ac9308f50054a153bd383e7a9a
lullaleophard <i12mosh@hotmail.it>
2024-03-08 10:53:04 +0100

    Create CandidateCommercialDataCtr.java
    ctr nuovo

---
+++ ProjectWorkMilanoBE/src/main/java/com/ctr/CandidateCommercialDataCtr.java
@@ -0,0 +1,86 @@
+package com.ctr;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.dao.CandidateCommercialDataRepository;
+import com.dao.CandidateRepository;
+import com.model.Candidate;
+import com.model.CandidateCommercialData;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping ("/CandidateCommercialDataCtr")
+public class CandidateCommercialDataCtr {
+	
+	@Autowired
+	private CandidateCommercialDataRepository candidateCommercialDataRep;
+	private CandidateRepository candidateRep;
+	
+//HOME
+	
+	@GetMapping("/home")
+	public String goHome(Model model,HttpServletRequest request) {
+		return "home";
+	}
+	
+//METODO AGGIUNGI
+	
+	@GetMapping("/addCandidateCommercialDataForm")
+    public String addCandidateCommercialDataForm(Model model,HttpServletRequest request){ //se ci arrivo direttamente dalla pagina candidato devo portarmi dietro l'id in ingresso
+		List <Candidate> candidateList = candidateRep.findAll();
+        model.addAttribute("candidates", candidateList); //se voglio inviare al form la lista di candidati da cui scegliere
+		return "addCandidateCommercialDataForm";
+	}            
+        
+    @PostMapping("/addCandidateCommercialData")
+    public String addCandidate(Model model, HttpServletRequest request, CandidateCommercialData candidateCommercialData) {
+    	candidateCommercialDataRep.save(candidateCommercialData);
+        return "saveSuccess";
+        
+	}
+    
+ //METODO AGGIORNA
+     
+    @GetMapping("/updateCandidateCommercialDataForm")
+    public String updateCandidateCommercialDataForm(Model model,HttpServletRequest request) {    	        
+    	return "updateCandidateCommercialDataForm";
+    	
+	}            
+   
+    @PostMapping("/updateCandidateCommercialData")
+    public String updateCandidateCommercialData(Model model, HttpServletRequest request, CandidateCommercialData candidateCommercialData) {
+    	candidateCommercialDataRep.save(candidateCommercialData);
+         return "updateSuccess";
+         
+    }
+    
+ //ELIMINA 
+    @PostMapping("/delete")
+    public String delete(Model model, HttpServletRequest request, Integer idCandidateCommercialData) {
+    	candidateCommercialDataRep.deleteById(idCandidateCommercialData);
+        return "deleteSuccess";
+        
+    }
+    
+ //RICERCA PER ID 
+    @GetMapping("/findByIdCandidateCommercialDataForm")
+    public String findByIdCandidateCommercialDataForm(Model model, HttpServletRequest request) {
+    	
+    	return "findByIdCandidateCommercialDataForm";
+    	
+    }
+    
+    @PostMapping("/findByIdCandidateCommercialData")
+    public String findByIdCandidateCommercialData(Model model, HttpServletRequest request, Integer idCandidateCommercialData) {
+    	candidateCommercialDataRep.findById(idCandidateCommercialData);
+    	return "findByIdCandidateCommercialData"; 
+    	
+    }
+
+}
